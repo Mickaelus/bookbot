db6ae1533d2b71f0872569d54124e7218b9e640c
avenak <mickael.magnan29@gmail.com>
2024-09-09 12:08:36 +0300

    cleaned up code

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()            #here we assign the entire text from the book to file_contents
-        print(file_contents)
-        words_count = 0                     # here we setup the word counting
-        words = file_contents.split()
-        for word in words:
-            words_count += 1
-        print(words_count)
+    file_path = "books/frankenstein.txt"
+    file_contents = get_file_contents(file_path)
+    num_words = get_num_words(file_contents)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(file_contents):   #gets the number of words in the book contents
+    words = file_contents.split()
+    return len(words)
+
+def get_file_contents(file_path):   #gets the whole book contents
+    with open(file_path) as f:
+        return f.read()
 
 if __name__ == "__main__":
     main()
