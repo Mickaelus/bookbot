a34b6fd2879d98bab8440ad6d495d24fc51e4d14
avenak <mickael.magnan29@gmail.com>
2024-09-09 11:28:38 +0300

    added word counting

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+        file_contents = f.read()            #here we assign the entire text from the book to file_contents
         print(file_contents)
+        words_count = 0                     # here we setup the word counting
+        words = file_contents.split()
+        for word in words:
+            words_count += 1
+        print(words_count)
+
 if __name__ == "__main__":
     main()
