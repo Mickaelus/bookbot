169914d133de07657143ad338a7ab4b20b0c9812
avenak <mickael.magnan29@gmail.com>
2024-09-09 16:18:05 +0300

    print report function added

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ def main():
     file_contents = get_file_contents(file_path)
     num_words = get_num_words(file_contents)
     num_characters = get_num_characters(file_contents)
-    sorted_characters = sorted(num_characters.items(), key=lambda x: x[1], reverse=True) #gets the number of character listed sorted in order of frequency
-    print(f"\n--- Begin report of {file_path} ---") #this is a header, to make the separation between the word count and the character count easier to see
-    print(f"{num_words} words found in the document")
-    for character, count in sorted_characters: 
-            print(f"The '{character}' character was found {count} times")
-    print("\n--- End report ---")
+    finished_report = print_report(file_path, num_words, num_characters)
+    print(finished_report)
 
 def get_num_words(file_contents):   #gets the number of words in the book contents
     words = file_contents.split()
@@ -23,7 +19,16 @@ def get_num_characters(file_contents): #gets a count of each character
     for character in file_contents.lower():
         if character.isalpha(): #checks if the character is a letter, used to sort out specific characters such as ? or !
             characters[character] = characters.get(character, 0) + 1 #adds the character to the dictionary if it doesn't already exist, or add one to the count if it does
+    characters = sorted(characters.items(), key=lambda x: x[1], reverse=True)
     return characters
+
+def print_report(file_path, num_words, num_characters):
+    report = f"--- Begin report of {file_path} ---\n"
+    report += f"{num_words} words found in the document\n\n"
+    for character, count in num_characters:
+        report += f"The '{character}' character was found {count} times\n"
+    report = report + "--- End report ---"
+    return report
 
 if __name__ == "__main__":
     main()
